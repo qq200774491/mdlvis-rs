a40a2e97b0d632fdf8fb5da1d9af38bbf0ca0171
nazarpunk <nazarpunk@gmail.com>
2026-03-16 19:58:19 +0200

    up

--- test.sh
+++ test.sh
@@ -8,6 +8,7 @@ cargo build --release
 
 MODEL_PATH="test-data/Arthas.mdx"
 MODEL_PATH="test-data/Ember Forge  Ember Knight/Ember Knight/Ember Knight_opt2.mdx"
+MODEL_PATH="test-data/QDMR.mdx"
 
 echo "Running with Arthas test model..."
 ./target/release/mdlvis-rs "$MODEL_PATH"
